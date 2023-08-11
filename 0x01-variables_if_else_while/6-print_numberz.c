5d12fc4f89d11fc419051bf3d8936f2b570df1f0
Prudence Wambui <wambuiprudy@gmail.com>
2023-08-11 03:44:20 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints all single base 10 numbers from 0
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
