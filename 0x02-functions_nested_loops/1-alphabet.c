37932c0c07c75927e570b7a5c7f8dad46e8640f9
Prudence Wambui <wambuiprudy@gmail.com>
2023-08-15 06:42:46 -0700

    correction 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * main - Entry point
- * Description: Prints the lowercase alphabet
+ * print_alphabet - Prints the alphabet in lowercase
  * Return: Always 0
  */
 void print_alphabet(void)
