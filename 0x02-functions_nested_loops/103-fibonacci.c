2d8a35f2b67c5859f09f5d9e57554a246d1e5cba
Prudence Wambui <wambuiprudy@gmail.com>
2023-08-15 17:17:21 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,17 +6,18 @@
 int main(void)
 {
 	int i = 0;
-	long j = 1, k = 2, sum = 2;
+	long j = 1, k = 2, sum = 0;
 
-	while (k + j < 4000000)
+	while (k < 4000000)
 	{
-		k = k + j;
 		if (k % 2 == 0)
 		{
 			sum += k;
-			j = k - j;
 		}
-		i++;
+		long next_term = j + k;
+
+		j = k;
+		k = next_term;
 	}
 	printf("%ld\n", sum);
 	return (0);
