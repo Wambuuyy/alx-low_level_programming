24c269c0a525b7b37d83d6874e0e719acbe9991d
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2023-09-28 17:51:38 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	unsigned int answer = 0;
 
 	if (b == NULL)
 		return (0);
@@ -19,8 +19,8 @@ unsigned int binary_to_uint(const char *b)
 		{
 			return (0);
 		}
-		result = (result << 1) + (*b - '0');
+		answer = (answer << 1) + (*b - '0');
 		b++;
 	}
-	return (result);
+	return (answer);
 }
