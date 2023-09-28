638f5d18bc3648e32e9a5fc108fdac55878ec40b
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2023-09-28 17:45:00 +0300

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,13 +9,13 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor_result = n ^ m;
+	unsigned long int result_xor = n ^ m;
 	unsigned int count = 0;
 
-	while (xor_result > 0)
+	while (result_xor > 0)
 	{
-		count += xor_result & 1;
-		xor_result >>= 1;
+		count += result_xor & 1;
+		result_xor >>= 1;
 	}
 	return (count);
 }
