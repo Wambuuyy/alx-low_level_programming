5341bfbc8279edb5ed581dcff940296be1f49c0b
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2023-10-03 02:02:32 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int p, d, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,13 +22,13 @@ int append_text_to_file(const char *filename, char *text_content)
 		len++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	p = open(filename, O_WRONLY | O_APPEND);
+	d = write(p, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (p == -1 || d == -1)
 		return (-1);
 
-	close(o);
+	close(p);
 
 	return (1);
 }
