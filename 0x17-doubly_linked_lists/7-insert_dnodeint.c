a7750c12f57764223ea0d2761e2dd5aa87276a4e
Prudence Wambui <wambuiprudy@gmail.com>
2023-12-08 13:32:16 +0300

    trial 2

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,52 +9,44 @@
  * @n: The data to add to new node.
  * Return: A pointer to new element, or NULL on failure.
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node = NULL, *temp = NULL;
-    unsigned int i = 0;
+	dlistint_t *new_node = NULL, *temp = NULL;
+	unsigned int i = 0;
 
-    if (!h)
-        return (NULL);
-
-    new_node = malloc(sizeof(dlistint_t));
-    if (!new_node)
-        return (NULL);
-    
-    new_node->n = n;
-    new_node->next = NULL;
-    new_node->prev = NULL;
-
-    if (!*h || idx == 0)
-    {
-        if (*h)
-            (*h)->prev = new_node;
-        new_node->next = *h;
-        *h = new_node;
-        return (new_node);
-    }
-
-    temp = *h;
-    while (i < idx - 1 && temp)
-    {
-        temp = temp->next;
-        i++;
-    }
-
-    if (!temp)
-    {
-        free(new_node);
-        return (NULL); // Index out of bounds
-    }
-
-    new_node->next = temp->next;
-    new_node->prev = temp;
-
-    if (temp->next)
-        temp->next->prev = new_node;
-
-    temp->next = new_node;
-
-    return (new_node);
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	new_node->n = n;
+	if (!h || !(*h))
+		*h = new_node;
+	else
+	{
+		temp = *h;
+		while (idx != i++ && temp->next)
+			temp = temp->next;
+		if (temp->next)
+			new_node->prev = temp->prev;
+		else
+			new_node->prev = temp;
+		if (idx == i)
+			temp->next = new_node, new_node->prev = temp;
+		else if (idx == i - 1)
+		{
+			if (temp->prev)
+				temp->prev->next = new_node;
+			else
+				*h = new_node;
+			temp->prev = new_node;
+			new_node->next = temp;
+		}
+		else
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	return (new_node);
 }
