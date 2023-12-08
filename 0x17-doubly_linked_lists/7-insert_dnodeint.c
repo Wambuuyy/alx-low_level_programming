adc26b700f8302968c6e54191e913e87354338d9
Prudence Wambui <wambuiprudy@gmail.com>
2023-12-08 13:59:11 +0300

    trial 2

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,56 +2,48 @@
 #include "lists.h"
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node = NULL, *temp = NULL;
-    unsigned int i = 0;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *temp = NULL;
+	unsigned int i = 0;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (!new_node)
-        return (NULL);
-    
-    new_node->n = n;
-    new_node->next = NULL;
-    new_node->prev = NULL;
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	if (!h || !(*h))
+	{
+		*h = new_node;
+		return (new_node);
+	}
+	temp = *h;
+	while (idx != i && temp)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (idx > i)
+	{
+		free(new_node);
+		return (NULL); /*Index out of bounds*/
+	}
+	if (temp)
+	{
+		new_node->prev = temp->prev;
+		new_node->next = temp;
+		if (temp->prev)
+			temp->prev->next = new_node;
+		else
+			*h = new_node;
+		temp->prev = new_node;
+	}
+	else
+	{
+		temp = *h;
+		while (temp->next)
+			temp = temp->next;
 
-    if (!h || !(*h))
-    {
-        *h = new_node;
-        return (new_node);
-    }
-
-    temp = *h;
-    while (idx != i && temp)
-    {
-        temp = temp->next;
-        i++;
-    }
-
-    if (idx > i)
-    {
-        free(new_node);
-        return (NULL); /*Index out of bounds*/
-    }
-
-    if (temp)
-    {
-        new_node->prev = temp->prev;
-        new_node->next = temp;
-        if (temp->prev)
-            temp->prev->next = new_node;
-        else
-            *h = new_node;
-        temp->prev = new_node;
-    }
-    else
-    {
-        temp = *h;
-        while (temp->next)
-            temp = temp->next;
-
-        new_node->prev = temp;
-        temp->next = new_node;
-    }
-
-    return (new_node);
+		new_node->prev = temp;
+		temp->next = new_node;
+	}
+	return (new_node);
 }
-
