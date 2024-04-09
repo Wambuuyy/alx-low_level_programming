7164069bb4cdf26d48af2cfae2d23b8658b99578
Wambuuyy <wambuiprudy@gmail.com>
2024-04-09 08:44:06 +0300

    remove white space from header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -30,7 +30,6 @@ typedef struct listint_s
  * @express: Pointer to the next node in the express lane
  *
  * Description: singly linked list node structure with an express lane
- * 
  */
 typedef struct skiplist_s
 {
